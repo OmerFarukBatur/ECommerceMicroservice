8be2318b7946725fe699684d24ae6beb5239fdf8
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2025-09-17 10:21:46 +0300

    implement repositories for Product and Category

---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public CategoryRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetAll() =>
+            _context.Categories.ToList();
+
+        public Category? GetById(int id) =>
+            _context.Categories.FirstOrDefault(p => p.Id == id);
+
+        public void Add(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ECommerceDbContext _context;
+
+        public ProductRepository(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll() =>
+            _context.Products.Include(p => p.Category).ToList();
+
+        public Product? GetById(int id) =>
+            _context.Products.Include(p => p.Category).FirstOrDefault(p => p.Id == id);
+
+        public void Add(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
