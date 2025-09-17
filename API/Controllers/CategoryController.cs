69cc9e2818780bcda23b19a4288a5e49d2667ea9
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2025-09-17 10:34:03 +0300

    add ProductController and CategoryController

---
+++ API/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using Application.Services;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryService _service;
+
+        public CategoryController(CategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAllCategories());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var category = _service.GetCategoryById(id);
+            return category == null ? NotFound() : Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _service.CreateCategory(category);
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Category category)
+        {
+            if (id != category.Id) return BadRequest();
+            _service.UpdateCategory(category);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.DeleteCategory(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Application.Services;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _service;
+
+        public ProductController(ProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAllProducts());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _service.GetProductById(id);
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _service.CreateProduct(product);
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+            if (id != product.Id) return BadRequest();
+            _service.UpdateProduct(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.DeleteProduct(id);
+            return NoContent();
+        }
+    }
+}
