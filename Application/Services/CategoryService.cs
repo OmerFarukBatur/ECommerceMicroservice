4b36a53ed8a4cb447c4223afcc212a1ee722fffc
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2025-09-17 10:28:46 +0300

    implement ProductService and CategoryService

---
+++ Application/Services/CategoryService.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Application.Services
+{
+    public class CategoryService
+    {
+        private readonly ICategoryRepository _repo;
+
+        public CategoryService(ICategoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<Category> GetAllCategories() => _repo.GetAll();
+        public Category? GetCategoryById(int id) => _repo.GetById(id);
+
+        public void CreateCategory(Category category)
+        {
+            _repo.Add(category);
+        }
+
+        public void UpdateCategory(Category category) => _repo.Update(category);
+        public void DeleteCategory(int id) => _repo.Delete(id);
+    }
+}
---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Application.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductService(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<Product> GetAllProducts() => _repo.GetAll();
+        public Product? GetProductById(int id) => _repo.GetById(id);
+
+        public void CreateProduct(Product product)
+        {
+            if (product.Price <= 0)
+                throw new ArgumentException("Price must be greater than zero.");
+
+            _repo.Add(product);
+        }
+
+        public void UpdateProduct(Product product) => _repo.Update(product);
+        public void DeleteProduct(int id) => _repo.Delete(id);
+    }
+}
