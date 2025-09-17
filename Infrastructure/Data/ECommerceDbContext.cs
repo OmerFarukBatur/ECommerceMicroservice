0bcd2415b7aa4cc15b8a7602c30519ecb2d1af51
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2025-09-17 09:59:04 +0300

    add ECommerceDbContext and EF Core configuration

---
+++ Infrastructure/Data/ECommerceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+            : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+            modelBuilder.Entity<Category>()
+                        .HasMany(c => c.Products)
+                        .WithOne(p => p.Category)
+                        .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
