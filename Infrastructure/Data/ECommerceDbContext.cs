77154f53cebd959a370f4030ab8f72bd3f76c914
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2025-09-17 11:14:08 +0300

    add EF Core packages (SqlServer, Design, Tools) and configure entity properties and relationships

--- Infrastructure/Data/ECommerceDbContext.cs
+++ Infrastructure/Data/ECommerceDbContext.cs
@@ -12,7 +12,31 @@ namespace Infrastructure.Data
         public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {            
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Id).UseIdentityColumn();
+                entity.Property(c => c.Name).HasMaxLength(250).IsRequired();
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Id).UseIdentityColumn();
+                entity.Property(p => p.Name).HasMaxLength(100).IsRequired();
+                entity.Property(p => p.Price).HasColumnType("decimal(18,2)");
+                entity.Property(p => p.Stock).IsRequired();
+
+                
+                entity.HasOne(p => p.Category)
+                      .WithMany(c => c.Products)
+                      .HasForeignKey(p => p.CategoryId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            
+
             modelBuilder.Entity<Category>()
                         .HasMany(c => c.Products)
                         .WithOne(p => p.Category)
